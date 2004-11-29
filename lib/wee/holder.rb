26ef7cbc146244e8ae64767c2b6b277d229bfa1b
mneumann <mneumann>
2004-11-29 23:04:41 +0000

    implement take and apply snapshot explicitly

--- lib/wee/holder.rb
+++ lib/wee/holder.rb
@@ -4,11 +4,19 @@ class Wee::ValueHolder
   def initialize(value=nil)
     @value = value
   end
+
+  def take_snapshot
+    [@value]
+  end
+
+  def apply_snapshot(snap)
+    @value = snap[0]
+  end
 end
 
 class Wee::StateHolder < Wee::ValueHolder
   def initialize(value)
     super
-    Wee::Session.current.state_registry << self
+    Wee::Session.current.register_object_for_backtracking(self)
   end
 end
