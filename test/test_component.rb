3008510f3720647c701b6506d7902f7a64b89126
mneumann <mneumann>
2004-12-16 16:35:39 +0000

    * added test case for Component#each_decoration and #remove_decoration_if

--- test/test_component.rb
+++ test/test_component.rb
@@ -57,4 +57,50 @@ class Test_Component < Test::Unit::TestCase
     assert_same c, c.decoration
     assert_nil  d3.owner
   end
+
+  def test_each_decoration
+    require 'enumerator'
+    c = Wee::Component.new
+    d1 = Wee::Decoration.new
+    d2 = Wee::Decoration.new
+    d3 = Wee::Decoration.new
+
+    c.add_decoration(d1)
+    c.add_decoration(d2)
+    c.add_decoration(d3)
+
+    assert_equal [d3, d2, d1], c.to_enum(:each_decoration).to_a
+  end
+
+  def test_remove_decoration_if
+    c = Wee::Component.new
+    d1 = Wee::Decoration.new
+    d2 = Wee::Decoration.new
+    d3 = Wee::Decoration.new
+    d4 = Wee::Decoration.new
+
+    c.add_decoration(d1)
+    c.add_decoration(d2)
+    c.add_decoration(d3)
+    c.add_decoration(d4)
+
+    def d1.a() end
+    def d3.a() end
+    c.remove_decoration_if {|d| d.respond_to?(:a)}
+    assert_equal [d4, d2], c.to_enum(:each_decoration).to_a
+
+    def d4.a() end
+    c.remove_decoration_if {|d| d.respond_to?(:a)}
+    assert_equal [d2], c.to_enum(:each_decoration).to_a
+
+    c.remove_decoration_if {|d| d.respond_to?(:a)}
+    assert_equal [d2], c.to_enum(:each_decoration).to_a
+
+    def d2.a() end
+    c.remove_decoration_if {|d| d.respond_to?(:a)}
+    assert_equal [], c.to_enum(:each_decoration).to_a
+
+    assert_equal c, c.decoration
+  end
+
 end
