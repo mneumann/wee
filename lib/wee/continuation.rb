bdd6ca0138e05100f7282ad8f8ab1caf937ed36f
mneumann <mneumann>
2005-01-26 13:05:40 +0000

    initial creation

---
+++ lib/wee/continuation.rb
@@ -0,0 +1,5 @@
+# Replace the non-continuation based implementation (which is loaded by
+# default) by a continuation enabled implementation.
+
+require 'wee/continuation/session'
+require 'wee/continuation/component'
