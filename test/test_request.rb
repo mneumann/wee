ef60cfbd4f7e34fad5b1a7a4af701653bf74b604
mneumann <mneumann>
2005-01-28 01:54:58 +0000

    fixed test case (as I changed the delimiter in Wee::Request)

--- test/test_request.rb
+++ test/test_request.rb
@@ -4,10 +4,11 @@ require 'wee/request'
 
 class Test_Request < Test::Unit::TestCase
   def test_parse
-    req = Wee::Request.new('/app', '/app/...@req_handler_id/page_id', nil, nil)
+    d = Wee::Request::DELIM
+    req = Wee::Request.new('/app', "/app/...#{d}req_handler_id/page_id", nil, nil, nil)
     assert_equal 'req_handler_id', req.request_handler_id
     assert_equal 'page_id', req.page_id
-    assert_equal '/app/@a/b?c', req.build_url('a', 'b', 'c')
-    assert_equal '/app/@req_handler_id/b', req.build_url('req_handler_id', 'b')
+    assert_equal "/app/#{d}a/b?c", req.build_url('a', 'b', 'c')
+    assert_equal "/app/#{d}req_handler_id/b", req.build_url('req_handler_id', 'b')
   end
 end
