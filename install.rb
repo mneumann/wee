bee8aba254384f286ca950c6b0197c0e735e4769
mneumann <mneumann>
2005-01-29 19:50:20 +0000

    include lib/skeleton/*/README files but omit .svn directories

--- install.rb
+++ install.rb
@@ -4,7 +4,7 @@ require 'ftools'
 # Install lib
 dst_dir = Config::CONFIG['sitelibdir']
 Dir.chdir('lib') { 
-  Dir['**/*.rb'].each {|file|
+  Dir['**/*'].reject {|f| f =~ /\.svn($|\/)/ or not File.file?(f) }.each {|file|
     File.mkpath File.join(dst_dir, File.dirname(file)), true
     File.install file, File.join(dst_dir, file), 0644, true
   }
