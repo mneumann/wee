cb4475022b822f9e20862aa8fa8472f9ab3ce3e9
Michael Neumann <mneumann@ntecs.de>
2009-02-11 15:33:39 +0100

    Surround by "module Wee"

--- lib/wee/core/valueholder.rb
+++ lib/wee/core/valueholder.rb
@@ -1,19 +1,24 @@
-# Implements a value holder. Useful for backtracking the reference assigned to
-# an instance variable (not the object itself!). An example where this is used
-# is the <tt>@__decoration</tt> attribute of class Wee::Component.
+module Wee
+  #
+  # Implements a value holder. Useful for backtracking the reference
+  # assigned to an instance variable (not the object itself!). An
+  # example where this is used is the <tt>@__decoration</tt> instance
+  # variable of class Wee::Component.
+  #
+  class ValueHolder 
+    attr_accessor :value
 
-class Wee::ValueHolder 
-  attr_accessor :value
+    def initialize(value=nil)
+      @value = value
+    end
 
-  def initialize(value=nil)
-    @value = value
-  end
+    def take_snapshot
+      @value
+    end
 
-  def take_snapshot
-    @value
-  end
+    def restore_snapshot(value)
+      @value = value
+    end
+  end # class ValueHolder
 
-  def restore_snapshot(value)
-    @value = value
-  end
-end
+end # module Wee
