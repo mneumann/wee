a7619c4e7e2e3b80171e6428115b521aa0c102c5
Michael Neumann <mneumann@ntecs.de>
2009-02-14 21:34:38 +0100

    Remove

--- lib/wee.rb
+++ lib/wee.rb
@@ -5,7 +5,18 @@ end
 
 module Wee::Examples; end
 
-require 'wee/core'
+require 'wee/valueholder'
+require 'wee/snapshot'
+require 'wee/callback'
+require 'wee/page'
+require 'wee/context'
+require 'wee/idgen'
+require 'wee/renderer'
+
+require 'wee/presenter'
+require 'wee/decoration'
+require 'wee/component'
+
 require 'wee/core_ext'
 
 require 'wee/application'
