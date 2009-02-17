5198ae0d57ba2e72e8c7dd8a311c8d10786128fd
Michael Neumann <mneumann@ntecs.de>
2009-02-17 11:01:29 +0100

    Indent Presenter

--- lib/wee/presenter.rb
+++ lib/wee/presenter.rb
@@ -1,63 +1,67 @@
-# Wee::Presenter is the superclass of all classes that want to participate in
-# rendering and callback-processing. Wee::Component and Wee::Decoration are
-# it's two most important subclasses.
+module Wee
 
-class Wee::Presenter
+  # Wee::Presenter is the superclass of all classes that want to participate in
+  # rendering and callback-processing. Wee::Component and Wee::Decoration are
+  # it's two most important subclasses.
 
-  # This method renders the content of the presenter.
-  #
-  # *OVERWRITE* this method in your own presenter classes to implement the
-  # view. By default this method does nothing!
-  #
-  # Use the current renderer as returned by #renderer or it's short-cut #r.
+  class Presenter
 
-  def render(r)
-  end
+    # This method renders the content of the presenter.
+    #
+    # *OVERWRITE* this method in your own presenter classes to implement the
+    # view. By default this method does nothing!
+    #
+    # Use the current renderer as returned by #renderer or it's short-cut #r.
 
-  # Render the presenter in the given rendering context. <b>DO NOT</b>
-  # overwrite this method, unless you know exactly what you're doing!
-  #
-  # Creates a new renderer object of the class returned by method
-  # #renderer_class, makes this the current renderer, then invokes method
-  # #render.
-  #
-  # [+context+]
-  #    An object of class Context
-
-  def render_on(context)
-    render(renderer_class.new(context, self))
-  end
-
-  # Dummy implementation. See Component#backtrack_state for more information. 
-  #
-  # [+snapshot+]
-  #    An object of class Snapshot
-
-  def backtrack_state(snapshot)
-  end
-
-  def process_callbacks(callbacks)
-    callbacks.input_callbacks.each_triggered(self) do |callback, value|
-      callback.call(value)
+    def render(r)
     end
 
-    callbacks.action_callbacks.each_triggered(self) do |callback, value|
-      callback.call
-      # TODO: return to main loop
+    # Render the presenter in the given rendering context. <b>DO NOT</b>
+    # overwrite this method, unless you know exactly what you're doing!
+    #
+    # Creates a new renderer object of the class returned by method
+    # #renderer_class, makes this the current renderer, then invokes method
+    # #render.
+    #
+    # [+context+]
+    #    An object of class Context
+
+    def render_on(context)
+      render(renderer_class.new(context, self))
     end
-  end
 
-  protected
+    # Dummy implementation. See Component#backtrack_state for more information. 
+    #
+    # [+snapshot+]
+    #    An object of class Snapshot
 
-  # Returns the class used as renderer for this presenter. Overwrite this
-  # method if you want to use a different renderer.
-  #
-  # Returned class must be a subclass of Wee::Renderer.
-  # 
-  # NEEDS TO BE OVERWRITTEN by some non-core files. 
+    def backtrack_state(snapshot)
+    end
 
-  def renderer_class
-    raise "Method renderer_class needs to be implemented!"
-  end
+    def process_callbacks(callbacks)
+      callbacks.input_callbacks.each_triggered(self) do |callback, value|
+        callback.call(value)
+      end
 
-end
+      callbacks.action_callbacks.each_triggered(self) do |callback, value|
+        callback.call
+        # TODO: return to main loop
+      end
+    end
+
+    protected
+
+    # Returns the class used as renderer for this presenter. Overwrite this
+    # method if you want to use a different renderer.
+    #
+    # Returned class must be a subclass of Wee::Renderer.
+    # 
+    # NEEDS TO BE OVERWRITTEN by some non-core files. 
+
+    def renderer_class
+      raise "Method renderer_class needs to be implemented!"
+    end
+
+  end # class Presenter
+
+end # module Wee
