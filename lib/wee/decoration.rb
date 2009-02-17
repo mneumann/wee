a3786393a95dbf9060d94951f48b0ff9150e919b
Michael Neumann <mneumann@ntecs.de>
2009-02-17 12:54:18 +0100

    style

--- lib/wee/decoration.rb
+++ lib/wee/decoration.rb
@@ -30,40 +30,45 @@ module Wee
   #
   class Decoration < Presenter
 
+    #
     # Points to the next decoration in the chain. A decoration is responsible for
     # all decorations or components "below" it (everything that follows this
     # decoration in the chain). In other words, it's the owner of everything
     # "below" itself.
-
+    #
     attr_accessor :next
 
+    #
     # Is this decoration a global or a local one? By default all decorations are
     # local unless this method is overwritten.
     #
     # A global decoration is added in front of the decoration chain, a local
     # decoration is added in front of all other local decorations but after all
     # global decorations.
-
+    #
     def global?() false end
 
+    #
     # Forwards method call to the next decoration in the chain.
-
+    #
     def process_callbacks(callbacks)
       @next.process_callbacks(callbacks)
     end
 
+    #
     # Forwards method call to the next decoration in the chain.
-
+    #
     def render_on(context)
       @next.render_on(context)
     end
 
+    #
     # We have to save the @next attribute to be able to correctly backtrack
     # calls, as method Wee::Component#call modifies it in the call to
     # <tt>component.remove_decoration(answer)</tt>. Removing the
     # answer-decoration has the advantage to be able to call a component more
     # than once!
-
+    #
     def backtrack(state)
       @next.backtrack(state)
       state.add_ivar(self, :@next, @next)
@@ -75,8 +80,9 @@ module Wee
 
     attr_accessor :decoration
 
+    #
     # Iterates over all decorations (note that the component itself is excluded). 
-
+    #
     def each_decoration # :yields: decoration
       d = @decoration
       while d and d != self
@@ -85,6 +91,7 @@ module Wee
       end
     end
     
+    #
     # Adds decoration +d+ to the decoration chain.
     #
     # A global decoration is added in front of the decoration chain, a local
@@ -92,7 +99,7 @@ module Wee
     # global decorations.
     #
     # Returns: +self+
-
+    #
     def add_decoration(d)
       if d.global?
         d.next = @decoration
@@ -120,11 +127,12 @@ module Wee
       return self
     end
 
+    #
     # Remove decoration +d+ from the decoration chain. 
     # 
     # Returns the removed decoration or +nil+ if it did not exist in the
     # decoration chain.
-
+    #
     def remove_decoration(d)
       if d == @decoration  # 'd' is in front
         @decoration = d.next
@@ -143,13 +151,13 @@ module Wee
       return d
     end
 
+    #
     # Remove all decorations that match the block condition.
     # 
     # Example (removes all decorations of class +HaloDecoration+):
     # 
     #   remove_decoration_if {|d| d.class == HaloDecoration}
     #
-
     def remove_decoration_if # :yields: decoration
       to_remove = []
       each_decoration {|d| to_remove << d if yield d}
@@ -160,37 +168,42 @@ module Wee
 
   #
   # A Wee::Delegate breaks the decoration chain and forwards the methods
-  # #process_callbacks, #render_on and #backtrack to the corresponding
-  # *chain* method of it's _delegate_ component (a Wee::Component).
+  # #process_callbacks, #render_on and #backtrack to the corresponding *chain*
+  # method of it's _delegate_ component (a Wee::Component).
   #
   class Delegate < Decoration
+
     def initialize(delegate)
       @delegate = delegate
     end
 
+    #
     # Forwards method to the corresponding top-level *chain* method of the
     # _delegate_ component.
-
+    #
     def process_callbacks(callbacks)
       @delegate.decoration.process_callbacks(callbacks)
     end
 
+    #
     # Forwards method to the corresponding top-level *chain* method of the
     # _delegate_ component.
-
+    #
     def render_on(context)
       @delegate.decoration.render_on(context)
     end
 
+    #
     # Forwards method to the corresponding top-level *chain* method of the
-    # _delegate_ component. We also take snapshots of all non-visible components,
-    # thus we follow the @next decoration (via super).
-
+    # _delegate_ component. We also take snapshots of all non-visible
+    # components, thus we follow the @next decoration (via super).
+    #
     def backtrack(state)
       super
       @delegate.decoration.backtrack(state)
     end
-  end
+
+  end # class DelegateDecoration
 
   #
   # A Wee::AnswerDecoration is wrapped around a component that will call
@@ -200,11 +213,12 @@ module Wee
   #
   class AnswerDecoration < Decoration
 
+    #
     # When a component answers, <tt>on_answer.call(args)</tt> will be executed
     # (unless nil), where +args+ are the arguments passed to Component#answer.
-    # Note that no snapshot of on_answer is taken, so you should avoid modifying
-    # it!
-
+    # Note that no snapshot of on_answer is taken, so you should avoid
+    # modifying it!
+    #
     attr_accessor :on_answer
 
     def process_callbacks(callbacks)
