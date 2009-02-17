9ffdc545f2de73eecbf215d35aa557f22c02610f
Michael Neumann <mneumann@ntecs.de>
2009-02-18 00:14:18 +0100

    Indent

--- lib/wee/html_writer.rb
+++ lib/wee/html_writer.rb
@@ -1,80 +1,85 @@
 require 'cgi'
 
-# A class used to write out HTML documents easily.
-#
-# Usage:
-#
-#   w = Wee::HtmlWriter.new(doc='')
-#   w.start_tag('html')
-#   w.start_tag('body')
-#   w.start_tag('a', 'href' => 'http://...')
-#   w.text('link')
-#   w.end_tag('a')
-#   w.end_tag('body')
-#   w.end_tag('html')
-#
-#   p doc        # => '<html><body><a href="http://...">link</a></body></html>'
-#
+module Wee
 
-class Wee::HtmlWriter
-  attr_accessor :port
+  #
+  # A class used to write out HTML documents easily.
+  #
+  # Usage:
+  #
+  #   w = Wee::HtmlWriter.new(doc='')
+  #   w.start_tag('html')
+  #   w.start_tag('body')
+  #   w.start_tag('a', 'href' => 'http://...')
+  #   w.text('link')
+  #   w.end_tag('a')
+  #   w.end_tag('body')
+  #   w.end_tag('html')
+  #
+  #   p doc        # => '<html><body><a href="http://...">link</a></body></html>'
+  #
 
-  def initialize(port)
-    @port = port 
-  end
+  class Wee::HtmlWriter
+    attr_accessor :port
 
-  def start_tag(tag, attributes=nil)
-    if attributes
-      @port << "<#{ tag }"
-      attributes.each {|k, v| 
-        if v
-          @port << %[ #{ k }="#{ v }"] 
-        else
-          @port << %[ #{ k }] 
-        end
-      }
-      @port << ">"
-    else
-      @port << "<#{ tag }>"
+    def initialize(port)
+      @port = port 
     end
 
-    self
-  end
+    def start_tag(tag, attributes=nil)
+      if attributes
+        @port << "<#{ tag }"
+        attributes.each {|k, v| 
+          if v
+            @port << %[ #{ k }="#{ v }"] 
+          else
+            @port << %[ #{ k }] 
+          end
+        }
+        @port << ">"
+      else
+        @port << "<#{ tag }>"
+      end
 
-  def single_tag(tag, attributes=nil)
-    if attributes
-      @port << "<#{ tag }"
-      attributes.each {|k, v| 
-        if v
-          @port << %[ #{ k }="#{ v }"] 
-        else
-          @port << %[ #{ k }] 
-        end
-      }
-      @port << " />"
-    else
-      @port << "<#{ tag } />"
+      self
     end
 
-    self
+    def single_tag(tag, attributes=nil)
+      if attributes
+        @port << "<#{ tag }"
+        attributes.each {|k, v| 
+          if v
+            @port << %[ #{ k }="#{ v }"] 
+          else
+            @port << %[ #{ k }] 
+          end
+        }
+        @port << " />"
+      else
+        @port << "<#{ tag } />"
+      end
+
+      self
+    end
+
+    def end_tag(tag)
+      @port << "</#{ tag }>"
+
+      self
+    end
+
+    def text(str)
+      @port << str.to_s
+
+      self
+    end
+    alias << text
+
+    def encode_text(str)
+      @port << CGI.escapeHTML(str.to_s)
+
+      self
+    end
   end
 
-  def end_tag(tag)
-    @port << "</#{ tag }>"
-
-    self
-  end
-
-  def text(str)
-    @port << str.to_s
-
-    self
-  end
-  alias << text
-
-  def encode_text(str)
-    @port << CGI.escapeHTML(str.to_s)
-
-    self
-  end
-end
+end # module Wee
