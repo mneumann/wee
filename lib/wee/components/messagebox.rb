fd1be4caca87ac85f052f44912c4370cfdf33644
Michael Neumann <mneumann@ntecs.de>
2009-02-24 17:50:56 +0100

    remove call to backtrack_decoration
    Method backtrack_decoration is called by default since
    a few commits, so there is no longer a need to call it here.

--- lib/wee/components/messagebox.rb
+++ lib/wee/components/messagebox.rb
@@ -4,10 +4,6 @@ class Wee::MessageBox < Wee::Component
     @text = text 
   end
 
-  def backtrack(state)
-    backtrack_decoration(state)
-  end
-
   def render(r)
     r.bold(@text)
     r.form do 
