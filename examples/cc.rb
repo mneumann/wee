7bd0aa34e96cfbcee54074fb0ead9d6606f14498
Michael Neumann <mneumann@ntecs.de>
2009-02-24 17:50:41 +0100

    Fix examples

--- examples/cc.rb
+++ examples/cc.rb
@@ -1,74 +1,28 @@
-$LOAD_PATH.unshift << "../lib"
+$LOAD_PATH.unshift "../lib"
+require 'rubygems'
 require 'wee'
-require 'wee/continuation'
-
-class MessageBox < Wee::Component
-  def initialize(text)
-    super()
-    @text = text 
-  end
-
-  def render
-    r.break
-    r.text(@text)
-    r.form do 
-      r.submit_button.value('OK').callback(:answer, true)
-      r.space
-      r.submit_button.value('Cancel').callback(:answer, false)
-    end
-    r.break
-  end
-end
-
-class MessageBox2 < Wee::Component
-  def initialize(text)
-    super()
-    @text = text 
-  end
-
-  def render
-    r.break
-    r.text(@text)
-    r.form do 
-      r.submit_button.value('OK')
-      r.space
-      r.submit_button.value('Cancel')
-    end
-    r.break
-  end
-end
+require 'wee/components/messagebox'
 
 class MainPage < Wee::Component
 
   def initialize
-    super()
-    @msgbox = MessageBox2.new("hallllo")
-    children << @msgbox
+    super
+    add_decoration(Wee::PageDecoration.new("Test"))
   end
 
   def click
-    if call MessageBox.new('Really quit?')
-      call MessageBox.new('You clicked YES')
+    if callcc Wee::MessageBox.new('Really quit?')
+      callcc Wee::MessageBox.new('You clicked YES')
     else
-      call MessageBox.new('You clicked Cancel')
-      call MessageBox.new('super')
+      callcc Wee::MessageBox.new('You clicked Cancel')
+      callcc Wee::MessageBox.new('super')
     end
   end
 
-  def render
-    r.page.title("Draw Test").with do 
-
-      r.break
-      r.anchor.callback(:click).with('show')
-    end 
+  def render(r)
+    r.anchor.callback_method(:click).with('show')
   end
 
 end
 
-if __FILE__ == $0
-  require 'wee/adaptors/webrick' 
-  require 'wee/utils'
-
-  app = Wee::Utils.app_for(MainPage)
-  Wee::WEBrickAdaptor.register('/app' => app).start 
-end
+Wee.runcc(MainPage)
--- examples/test.rb
+++ examples/test.rb
@@ -1,39 +1,22 @@
-#!/usr/bin/env ruby
-$LOAD_PATH.unshift << "../lib"
+$LOAD_PATH.unshift "../lib"
+require 'rubygems'
 require 'wee'
 require 'demo/counter'
 
 class MainPage < Wee::Component
   def initialize
     super
-    @counters = (1..10).map {|i| Counter.new(i)}
-    children.push(*@counters)
+    @counters = (1..10).map {|i| add_child Counter.new(i)}
+    add_decoration(Wee::PageDecoration.new("Test"))
   end
 
-  def render
+  def render(r)
     r.page.title("Counter Test").with do 
       @counters.each { |cnt| r.render(cnt); r.break  }
     end
   end
 end
 
-class MySession < Wee::Session
-  def initialize
-    super do
-      self.root_component = MainPage.new
-      self.page_store = Wee::Utils::LRUCache.new(10) # backtrack up to 10 pages
-      self.expire_after = 60
-    end
-  end
-end
-
 if __FILE__ == $0
-  require 'wee/utils'
-  require 'wee/adaptors/webrick'
-  app = Wee::Application.new {|app|
-    app.default_request_handler { MySession.new }
-    app.id_generator = Wee::IdGenerator::Sequential.new(rand(1_000_000))
-    app.max_request_handlers = 2
-  }
-  Wee::WEBrickAdaptor.register('/app' => app).start
+  Wee.run(MainPage)
 end
