b5bf46ce1586333f4330c921a76bef2cefb0753f
Michael Neumann <mneumann@ntecs.de>
2009-03-19 21:39:23 +0100

    Add JQuery functionality

--- examples/ajax.rb
+++ examples/ajax.rb
@@ -2,8 +2,9 @@ $LOAD_PATH.unshift "../lib"
 require 'rubygems'
 require 'wee'
 require 'rack'
+require 'wee/jquery'
 
-class UpdateComponent < Wee::Component
+class AjaxCounter < Wee::Component
   def initialize
     super
     @counter = 0
@@ -15,49 +16,21 @@ class UpdateComponent < Wee::Component
   end
 
   def render(r)
-    r.div.id("wee_#{self.object_id}").with {
-      r.div.onclick_update_multi {
-        @counter += 1
-        redraw(r, self)
-      }.with(@counter.to_s)
-    }
-  end
-
-  def redraw(rold, *components)
-    r = Wee::Renderer.new
-    r.request   = rold.request
-    r.response  = Wee::Response.new
-    # replace all callbacks of those components 
-    r.callbacks = rold.callbacks # Wee::Callbacks.new
-    r.document  = Wee::HtmlWriter.new(r.response)
-
-    components.each do |c|
-      r.render_decoration(c)
-=begin
-      begin
-        c.decoration.render_on(r)
-      ensure
-        r.close
-      end
-=end
-    end
-
-    session.send_response(r.response)
+    r.div.oid.onclick_update_self_callback { @counter += 1 }.with(@counter.to_s)
   end
 end
 
 class HelloWorld < Wee::Component
-
   def initialize
     super
-    @cs = (1..10).map { add_child UpdateComponent.new }
+    @cs = (1..10).map { add_child AjaxCounter.new }
   end
 
   def render(r)
     r.html {
       r.head {
         r.title('Wee + Ajax')
-        r.javascript.src('/js/jquery-1.3.1.min.js')
+        Wee::JQuery.javascript_includes(r)
       }
       r.body {
         r.div.onclick_callback { p "refresh" }.with("Refresh")
@@ -65,20 +38,16 @@ class HelloWorld < Wee::Component
       }
     }
   end
-
 end
-
 
 if __FILE__ == $0
   require 'rack/handler/webrick'
   app = Rack::Builder.app do
+    Wee::JQuery.install('/jquery', self)
     map '/ajax' do
       run Wee::Application.new {
         Wee::Session.new(HelloWorld.new)
       }
-    end
-    map '/js' do
-      run Rack::File.new('.')
     end
   end
   Rack::Handler::WEBrick.run(app, :Port => 2000)
---
+++ lib/wee/jquery.rb
@@ -0,0 +1,1 @@
+require 'wee/jquery/jquery'
