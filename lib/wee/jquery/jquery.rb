4fa824eb09ed7e60b02e55edfa0b9348d8ae1a2c
Michael Neumann <mneumann@ntecs.de>
2009-03-25 18:37:24 +0100

    Add missing jquery files

---
+++ lib/wee/jquery/jquery.rb
@@ -0,0 +1,17 @@
+module Wee
+  class JQuery
+    FILES = %w(jquery-1.3.2.min.js wee-jquery.js)
+    PUBLIC_DIR = File.expand_path(File.join(File.dirname(__FILE__), 'public'))
+
+    def self.install(mount_path, builder)
+      builder.map(@mount_path = mount_path) do
+        run Rack::File.new(PUBLIC_DIR)
+      end
+    end
+
+    def self.javascript_includes(r)
+      raise "JQuery.install needs to be called" unless @mount_path
+      FILES.each {|f| r.javascript.src("#{@mount_path}/#{f}") }
+    end
+  end
+end
