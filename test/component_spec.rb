ef35a227a74c56f90abb98ddafc7d54d473094fd
Michael Neumann <mneumann@mischa.(none)>
2009-10-26 19:26:57 +0100

    Spec

--- test/component_spec.rb
+++ test/component_spec.rb
@@ -13,3 +13,28 @@ describe Wee::Component, "when first created" do
     @component.decoration.should == @component 
   end
 end
+
+describe Wee::Component, "after adding one decoration" do
+  before do
+    @component = Wee::Component.new
+    @decoration = Wee::Decoration.new
+    @component.add_decoration(@decoration)
+  end
+
+  it "should point to the added decoration" do
+    @component.decoration.should == @decoration
+  end
+
+  it "the added decoration should point back to the component" do
+    @component.decoration.next.should == @component
+  end
+
+  it "should return decoration after removing it" do
+    @component.remove_decoration(@decoration).should == @decoration
+  end
+
+  it "should have no decoration after removing it" do
+    @component.remove_decoration(@decoration)
+    @component.decoration.should == @component 
+  end
+end
