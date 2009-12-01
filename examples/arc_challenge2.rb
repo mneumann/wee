2f8ab6466a4434bd0780353e5b596262dca2466b
Michael Neumann <mneumann@ntecs.de>
2009-12-02 00:34:49 +0100

    Remove extra file

--- examples/arc_challenge2.rb
+++ examples/arc_challenge2.rb
@@ -9,7 +9,28 @@
 $LOAD_PATH.unshift "../lib"
 require 'rubygems'
 require 'wee'
-require "io"
+require "wee/conversation"
+
+class Wee::IO
+  def initialize(component)
+    @component = component
+  end
+
+  def ask
+    @component.display do |r|
+      r.text_input.callback {|text| answer(text)}
+      r.submit_button.value("Enter")
+    end 
+  end
+
+  def pause(text)
+    @component.display {|r| r.anchor.callback { answer }.with(text) }
+  end
+
+  def tell(text)
+    @component.display {|r| r.text text.to_s }
+  end
+end
 
 class Conversation < Wee::Task
   def go
