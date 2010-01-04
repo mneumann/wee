e37da90ef7940ab57c0e505248a5c7a2adea2523
Michael Neumann <mneumann@ntecs.de>
2010-01-05 00:38:35 +0100

    *

--- examples/i18n/app.rb
+++ examples/i18n/app.rb
@@ -6,7 +6,7 @@ require 'wee/locale'
 class HelloWorld < Wee::RootComponent
   def render(r)
     r.h1 _("Hello World!")
-    r.select_list(%w(de en)).selected(session.locale).labels(["Deutsch", "English"]).callback {|lang| session.locale = lang}
+    r.select_list(%w(en de)).selected(session.locale).labels(["English", "Deutsch"]).callback {|lang| session.locale = lang}
     r.submit_button.value(_("Set"))
   end
 end
