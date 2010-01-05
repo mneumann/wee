542bb4e12693f44bf0a3548153b47f3dcde8bd0c
Michael Neumann <mneumann@ntecs.de>
2010-01-05 17:26:35 +0100

    Add simple render benchmark

---
+++ test/bm_render.rb
@@ -0,0 +1,34 @@
+#
+# Render Wee::HelloWorld n-times
+#
+
+$LOAD_PATH.unshift "./lib"
+require 'rubygems'
+require 'wee'
+require 'rack'
+
+class Rack::Request
+  def put?; get? end
+end
+
+class Wee::HtmlWriter
+  def join
+    @port
+  end
+end
+
+root_component = Wee::HelloWorld.new
+Integer(ARGV[0] || raise).times do
+  r = Wee::Renderer.new
+  r.request   = Wee::Request.new({'REQUEST_METHOD' => 'GET', 'SCRIPT_NAME' => 'blah', 'PATH_INFO' => 'blubb',
+  'QUERY_STRING' => '_p=blah&_s=session'})
+  r.document  = Wee::HtmlDocument.new
+  r.callbacks = Wee::Callbacks.new
+
+  begin
+    root_component.decoration.render_on(r)
+  ensure
+    r.close
+  end
+  Wee::GenericResponse.new(r.document.join)
+end
