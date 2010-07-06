3a571b4a518f8cef471ea0aa40910effd0fe9e71
Dominic Tarr <dominic.tarr@gmail.com>
2010-07-07 02:52:37 +1200

    changed html_document to produce human readable html

--- test/test_html_canvas.rb
+++ test/test_html_canvas.rb
@@ -1,22 +1,28 @@
+require 'rubygems'
 require 'test/unit'
 module Wee; end
 require 'wee/html_document'
 require 'wee/html_brushes'
 require 'wee/html_canvas'
+require 'wee/callback'
+require 'wee/request'
+require 'flexmock/test_unit'
 #require 'wee/context'
 
 class Test_HtmlCanvas < Test::Unit::TestCase
+def new_canvas (doc,cbs = nil,req = nil)
+   session=nil
+   request=req
+  response=nil
+ callbacks=cbs
+  document=doc
+current_component=nil
+   c = Wee::HtmlCanvas.new(session,request,response,callbacks,document,current_component)
+end
   def test_simple
 	
    doc = Wee::HtmlDocument.new
-   session=nil
-   request=nil
-  response=nil
- callbacks=nil
-  document=doc
-current_component=nil
-
-   c = Wee::HtmlCanvas.new(session,request,response,callbacks,document,current_component)
+   c = new_canvas(doc)
     c.form.action("foo").with {
       c.table {
         c.table_row.id("myrow").with {
@@ -25,23 +31,38 @@ current_component=nil
       }
       c.space
     }
+   puts doc.to_s
 
-   assert_equal %[<form method="POST" action="foo"><table><tr id="myrow"><td align="top">Hello world</td></tr></table>&nbsp;</form>], doc.to_s
+   assert_equal %[<form method="POST" action="foo"> <table> <tr id="myrow"> <td align="top">Hello world</td></tr></table>&nbsp;</form>], doc.to_s
+#USER REGEXP's TO COMPARE THIS TO OTHER OUTPUT HTML IGNORING WHITESPACE.
   end
-
 def test_wrong_brushname
    doc = Wee::HtmlDocument.new
-   session=nil
-   request=nil
-  response=nil
- callbacks=nil
-  document=doc
-current_component=nil
-
-   c = Wee::HtmlCanvas.new(session,request,response,callbacks,document,current_component)
+   c = new_canvas(doc)
+   begin
     c.form.action("foo").with {
 	c.password_field.value "hello"
     }
+   fail "expected exception"
+   rescue; end
 
 end
+def test_mock_callback
+   doc = Wee::HtmlDocument.new
+   cbs = Wee::Callbacks.new
+   req = flexmock(:build_url => "test/")
+
+#      cb = flexmock("callbacks")
+#	sensor.should_receive(:"respond_to?").times(1).
+#        and_return(10, 12, 14)
+
+	c = new_canvas(doc,cbs,req)
+	@callback = false
+	c.anchor.callback {@callback = true}.with("hello!")
+
+	puts cbs.inspect.split(" ").join("\n")
+	puts doc.to_s
 end
+
+end
+
